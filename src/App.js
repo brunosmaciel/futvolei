e2809a37dd92c7327e0e1c3d9142faba255f35bb
Bruno Maciel <brunosazanamaciel@gmail.com>
2022-07-26 18:56:29 -0300

    added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/button-has-type */
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 export default function App() {
